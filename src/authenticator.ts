e89fd99822adf16e5fae61aacc8c03c4bec0f706
Bernt-Johan Bergshaven <bernt@clarify.io>
2023-09-19 16:59:05 +0200

    Update auth and bump version

--- src/authenticator.ts
+++ src/authenticator.ts
@@ -1,17 +1,13 @@
-import { AccessToken, MillCredential } from "./client";
 import fetch from "node-fetch";
 import { type Agent } from "https";
-import { Result } from "./models/result";
 
-type AuthorizationCode = {
-  authorization_code: string;
-};
-
-interface TokenPayload {
+export interface AccessToken {
+  expires_in: number;
+  token_type: string;
   access_token: string;
   refresh_token: string;
-  expireTime: number;
-  refresh_expireTime: number;
+  /// Date in local time, when the access token expires
+  expiresAt: Date;
 }
 
 type AuthenticatorStatus =
@@ -31,26 +27,27 @@ type AuthenticatorStatus =
       error: unknown;
     };
 
-export class Authenticator {
+export class ClientCredentialsAuthenticator {
   private status: AuthenticatorStatus = { type: "initial" };
 
   constructor(
-    private credentials: MillCredential,
-    private apiURL: string,
+    private client_id: string,
+    private client_secret: string,
+    private authenticationURL: string,
     private agent?: Agent
   ) {}
 
   async authenticate() {
-    let accessToken = await this.retrieveAccessToken();
-    return [["access_token", `${accessToken.access_token}`]];
+    let accessToken = await this.retriveAccessToken();
+    return [["Authorization", `Bearer ${accessToken}`]];
   }
 
-  private async retrieveAccessToken(): Promise<AccessToken> {
+  private async retriveAccessToken(): Promise<AccessToken> {
     switch (this.status.type) {
       case "authenticating":
         return await this.status.result;
       case "authenticated":
-        if (new Date() < this.status.accessToken.expireTime) {
+        if (new Date() < this.status.accessToken.expiresAt) {
           return this.status.accessToken;
         }
     }
@@ -67,53 +64,22 @@ export class Authenticator {
   }
 
   private async refresh() {
-    let authCodeResponse = await fetch(`${this.apiURL}/share/applyAuthCode`, {
+    let response = await fetch(this.authenticationURL, {
       method: "POST",
       headers: {
         "Content-Type": "application/x-www-form-urlencoded",
-        access_key: this.credentials.access_key,
-        secret_token: this.credentials.secret_token,
-      },
-      agent: this.agent,
-    });
-    let codePayload =
-      (await authCodeResponse.json()) as Result<AuthorizationCode>;
-    if (codePayload.success === false) {
-      return Promise.reject(new Error(codePayload.message));
-    }
-    let response = await fetch(`${this.apiURL}/share/applyAccessToken`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-        authorization_code: codePayload.data.authorization_code,
       },
       body: new URLSearchParams({
-        username: this.credentials.username,
-        password: this.credentials.password,
+        grant_type: "password",
+        client_id: this.client_id,
+        client_secret: this.client_secret,
       }),
       agent: this.agent,
     });
-    let payload = (await response.json()) as Result<TokenPayload>;
-    if (payload.success === false) {
-      return Promise.reject(new Error(payload.message));
-    }
-    if (payload && payload && payload.data.expireTime) {
-      try {
-        let parts = payload.data.access_token.split(".");
-        let decoded = Buffer.from(parts[1], "base64").toString();
-        let accessToken = {
-          payload: JSON.parse(decoded),
-          access_token: payload.data.access_token,
-          refresh_token: payload.data.refresh_token,
-          expireTime: new Date(payload.data.expireTime),
-          refresh_expireTime: new Date(payload.data.refresh_expireTime),
-            Date.now() + payload.data.refresh_expireTime * 1000
-          ),
-        };
-        return accessToken;
-      } catch (error) {
-        return Promise.reject(error);
-      }
+
+    let payload = (await response.json()) as AccessToken;
+    if (payload && payload.access_token && payload.expires_in) {
+      payload.expiresAt = new Date(Date.now() + payload.expires_in * 1000);
     }
     return Promise.reject(new Error("Unknown payload"));
   }
--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ export { MillheatAPI as Client } from "./client";
 
 // Data structures
 export type { Home } from "./models/home-result";
+export type { DeviceInfoItem } from "./models/device-info-item";
+export type { DeviceItem } from "./models/device-item";
